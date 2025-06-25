0937f84d722a17e9c4ef39312f79b284da06f809
Bruno Oliveira <bruno.olvslv@gmail.com>
2025-06-25 01:08:53 -0300

    add pequenos ajustes e skills bar

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -7,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  icons: string[] = [
+    'devicon-html5-plain colored',
+    'devicon-css3-plain colored',
+    'devicon-angularjs-plain colored',
+    'devicon-javascript-plain colored',
+    'devicon-typescript-plain colored',
+    'devicon-csharp-plain colored',
+    'devicon-python-plain colored',
+    'devicon-microsoftsqlserver-plain colored',
+    'devicon-mysql-plain colored',
+    'devicon-jasmine-original colored'
+  ];
 
+  repeatCount = 1;
+
+  ngOnInit() {
+    this.calculateRepeatCount();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.calculateRepeatCount();
+  }
+
+  calculateRepeatCount() {
+    const iconWidth = 30 + 30;
+    const screenWidth = window.innerWidth;
+    const minWidthNeeded = screenWidth * 2;
+    const iconsPerRow = this.icons.length * iconWidth;
+    this.repeatCount = Math.ceil(minWidthNeeded / iconsPerRow);
+  }
 }
