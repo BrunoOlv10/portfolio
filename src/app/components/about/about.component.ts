23892a2833c1861bfc27c8e6980a996087768455
Bruno Oliveira <bruno.olvslv@gmail.com>
2025-07-09 22:24:40 -0300

    add e-mail com pop-up, número whats e navegação na navbar para home, projects e about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  copyEmail(event: MouseEvent): void {
+    const button = event.currentTarget as HTMLButtonElement;
+    const emailButton = button.closest(".email-button");
 
+    if (!emailButton) return;
+
+    const email = "bruno.olvslv@gmail.com";
+
+    navigator.clipboard.writeText(email).then(() => {
+        emailButton.classList.add("copied");
+
+        setTimeout(() => {
+            emailButton.classList.remove("copied");
+        }, 2000);
+    });
+  }
 }
