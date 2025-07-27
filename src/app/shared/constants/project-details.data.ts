aba44c14631a942c1886fa2670917178cc1de1b6
Bruno Oliveira <bruno.olvslv@gmail.com>
2025-07-26 23:46:51 -0300

    add alguns dados reais dos projetos

--- src/app/shared/constants/project-details.data.ts
+++ src/app/shared/constants/project-details.data.ts
@@ -2,112 +2,67 @@ import { ProjectDetails } from '../models/project.model';
 
 export const PROJECT_DETAILS: ProjectDetails[] = [
     {
-      title: 'Exemplo 1',
-      accessUrl: '#',
-      codeUrl: '#',
+      title: 'RevioScan',
+      accessUrl: 'https://revio-desafio-produtos.vercel.app',
+      codeUrl: 'https://github.com/BrunoOlv10/revio-desafio-produtos',
       screens: [
         {
           title: 'Login',
-          image: 'assets/projects/furia/tela-infos.png',
-          description: 'Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...Tela de login com autenticação...'
-          // description: 'Tela de login com autenticação...'
+          image: 'assets/images-projects/furia/tela-infos.png',
+          description: 'Tela de login com autenticação...'
         },
         {
           title: 'Home',
-          image: 'assets/projects/furia/tela-infos.png',
+          image: 'assets/images-projects/furia/tela-infos.png',
           description: 'Página inicial com chat...'
         },
         {
           title: 'Sobre',
-          image: 'assets/projects/furia/tela-infos.png',
+          image: 'assets/images-projects/furia/tela-infos.png',
           description: 'Página sobre...'
         },
       ]
     },
     {
-      title: 'Exemplo 2',
+      title: 'Furia Esports Chat',
       accessUrl: '#',
-      codeUrl: '#',
+      codeUrl: 'https://github.com/BrunoOlv10/furia-experiencia-conversacional',
       screens: [
         {
           title: 'Login',
-          image: 'assets/projects/furia/tela-infos.png',
+          image: 'assets/images-projects/furia/tela-infos.png',
           description: 'Tela de login com autenticação...'
         },
         {
           title: 'Home',
-          image: 'assets/projects/furia/tela-infos.png',
+          image: 'assets/images-projects/furia/tela-infos.png',
           description: 'Página inicial com chat...'
         },
         {
           title: 'Sobre',
-          image: 'assets/projects/furia/tela-infos.png',
+          image: 'assets/images-projects/furia/tela-infos.png',
           description: 'Página sobre...'
         },
       ]
     },
     {
-      title: 'Exemplo 3',
+      title: 'Buscador Operadoras',
       accessUrl: '#',
-      codeUrl: '#',
+      codeUrl: 'https://github.com/BrunoOlv10/intuitive-care-teste-tecnico',
       screens: [
         {
           title: 'Login',
-          image: 'assets/projects/furia/tela-infos.png',
+          image: 'assets/images-projects/furia/tela-infos.png',
           description: 'Tela de login com autenticação...'
         },
         {
           title: 'Home',
-          image: 'assets/projects/furia/tela-infos.png',
+          image: 'assets/images-projects/furia/tela-infos.png',
           description: 'Página inicial com chat...'
         },
         {
           title: 'Sobre',
-          image: 'assets/projects/furia/tela-infos.png',
-          description: 'Página sobre...'
-        },
-      ]
-    },
-    {
-      title: 'Exemplo 4',
-      accessUrl: '#',
-      codeUrl: '#',
-      screens: [
-        {
-          title: 'Login',
-          image: 'assets/projects/furia/tela-infos.png',
-          description: 'Tela de login com autenticação...'
-        },
-        {
-          title: 'Home',
-          image: 'assets/projects/furia/tela-infos.png',
-          description: 'Página inicial com chat...'
-        },
-        {
-          title: 'Sobre',
-          image: 'assets/projects/furia/tela-infos.png',
-          description: 'Página sobre...'
-        },
-      ]
-    },
-    {
-      title: 'Exemplo 5',
-      accessUrl: '#',
-      codeUrl: '#',
-      screens: [
-        {
-          title: 'Login',
-          image: 'assets/projects/furia/tela-infos.png',
-          description: 'Tela de login com autenticação...'
-        },
-        {
-          title: 'Home',
-          image: 'assets/projects/furia/tela-infos.png',
-          description: 'Página inicial com chat...'
-        },
-        {
-          title: 'Sobre',
-          image: 'assets/projects/furia/tela-infos.png',
+          image: 'assets/images-projects/furia/tela-infos.png',
           description: 'Página sobre...'
         },
       ]
--- src/app/shared/constants/projects.data.ts
+++ src/app/shared/constants/projects.data.ts
@@ -2,33 +2,21 @@ import { Project } from '../models/project.model';
 
 export const PROJECTS: Project[] = [
     {
-      title: 'Exemplo 1',
-      image: 'assets/projects/furia/tela-infos.png',
-      technologies: ['Angular', 'TypeScript', 'C#', 'SQL Server'],
+      title: 'RevioScan',
+      image: 'assets/images-projects/furia/tela-infos.png',
+      technologies: ['HTML', 'CSS', 'JavaScript', 'Python'],
+      accessUrl: 'https://revio-desafio-produtos.vercel.app'
+    },
+    {
+      title: 'Furia Esports Chat',
+      image: 'assets/images-projects/furia/tela-infos.png',
+      technologies: ['Angular', 'TypeScript', 'SCSS', 'Python'],
       accessUrl: '#'
     },
     {
-      title: 'Exemplo 2',
-      image: 'assets/projects/furia/tela-infos.png',
-      technologies: ['React', 'JavaScript', 'Python', 'MySQL'],
-      accessUrl: '#'
-    },
-    {
-      title: 'Exemplo 3',
-      image: 'assets/projects/furia/tela-infos.png',
-      technologies: ['Vue', 'TypeScript', 'Java'],
-      accessUrl: '#'
-    },
-    {
-      title: 'Exemplo 4',
-      image: 'assets/projects/furia/tela-infos.png',
-      technologies: ['Angular', 'JavaScript', 'C#', 'MySQL'],
-      accessUrl: '#'
-    },
-    {
-      title: 'Exemplo 5',
-      image: 'assets/projects/furia/tela-infos.png',
-      technologies: ['Angular', 'JavaScript', 'C#', 'MySQL'],
+      title: 'Buscador Operadoras',
+      image: 'assets/images-projects/furia/tela-infos.png',
+      technologies: ['Vue', 'JavaScript', 'Python'],
       accessUrl: '#'
     },
 ];
